2aaae81f53077d32cf293cfdfc64d6ca987f34ea
jackmford <fordjac@bvu.edu>
2022-01-31 17:10:04 -0600

    messing with logos n stuff

--- server.go
+++ server.go
@@ -12,9 +12,14 @@ func hello(w http.ResponseWriter, r *http.Request) {
     tpl.Execute(w, nil)
 }
 
+// public serves static assets such as CSS and JavaScript to clients.
+//func public() http.Handler {
+  //return http.StripPrefix("/static/", http.FileServer(http.Dir("./static")))
+  //}
+
 func main() {
     http.HandleFunc("/", hello)
-
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
     err := http.ListenAndServe(":8787", nil)
     if err != nil {
         log.Fatal(err)
