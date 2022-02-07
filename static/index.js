161a22c94bdf874a8bf2661aa3033aeb30a45259
jackmford <jackmitchellfordyce@gmail.com>
2022-02-07 16:51:34 -0600

    figuring out slideshows.. kinda

--- static/index.js
+++ static/index.js
@@ -1,1 +1,30 @@
-console.log("hey");
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  console.log(slides);
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  console.log(slideIndex);
+  console.log(slides);
+  slides[slideIndex-1].style.display = "inline";
+  dots[slideIndex-1].className += " active";
+
+}
